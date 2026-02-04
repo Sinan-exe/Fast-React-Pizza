9e7d35e45890b00519d359106c9b791b2b6e384a
Ismail Sinan <ismailsinan2003@gmail.com>
2026-02-04 21:04:56 +0400

    Styled the loader component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,9 +1,18 @@
-function Button({ children, disabled }) {
+import { Link } from "react-router-dom";
+
+function Button({ children, disabled, to }) {
+  const styles =
+    "inline-block rounded-lg bg-yellow-400 px-3 py-2 font-semibold tracking-wide text-stone-800 drop-shadow-sm transition-colors duration-300 hover:bg-yellow-300 sm:text-lg ";
+
+  if (to) {
+    return (
+      <Link to={to} className={styles}>
+        {children}
+      </Link>
+    );
+  }
   return (
-    <button
-      disabled={disabled}
-      className="inline-block rounded-lg bg-yellow-400 px-3 py-2 font-semibold tracking-wide text-stone-800 drop-shadow-sm transition-colors duration-300 hover:bg-yellow-300 sm:text-lg md:px-4 md:py-3"
-    >
+    <button disabled={disabled} className={styles}>
       {children}
     </button>
   );
